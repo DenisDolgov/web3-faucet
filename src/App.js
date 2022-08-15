300090b8cd3a0d0333cfbc3b7582438cf84cd5b3
Денис Долгов <denis.dolgov@monopoly.su>
2022-08-15 23:57:58 +0700

    show balance

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import './App.css';
 import {useEffect, useState} from "react";
 import Web3 from 'web3';
 import detectEthereumProvider from "@metamask/detect-provider";
+import {loadContract} from "./utils/loadContract";
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
     web3: null,
+    contract: null,
   });
   const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
 
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider();
+      const contract = await loadContract('Faucet', provider);
 
       if (provider) {
         setWeb3Api({
           web3: new Web3(provider),
           provider,
+          contract,
         });
       } else {
         console.error('Please, install Metamask.');
@@ -26,6 +31,16 @@ function App() {
 
     void loadProvider();
   }, []);
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance, 'ether'));
+    };
+
+    web3Api.contract && loadBalance();
+  }, [web3Api]);
 
   useEffect(() => {
     const getAccount = async () => {
@@ -46,13 +61,13 @@ function App() {
               account
                 ? <span>{account}</span>
                 : <button
-                    onClick={() => web3Api.provider.request({ method: 'eth_requestAccounts' })}
-                    className="button is-info mr-2"
-                  >Connect Wallet</button>
+                  onClick={() => web3Api.provider.request({method: 'eth_requestAccounts'})}
+                  className="button is-info mr-2"
+                >Connect Wallet</button>
             }
           </div>
           <div className="balance-view is-size-2 my-4">
-            Current balance: <strong>10</strong> ETH
+            Current balance: <strong>{balance}</strong> ETH
           </div>
           <button className="button is-link mr-2">Donate</button>
           <button className="button is-primary">Withdraw</button>
